f7ca91544fc2a8c6b2393d90e5c3717699a1c604
wangxinli <1158261925@qq.com>
2018-10-30 22:23:14 +0800

    完善testng的测试用例，增加了三个测试方法

--- src/main/java/com/test/two/NewTest.java
+++ src/main/java/com/test/two/NewTest.java
@@ -2,16 +2,38 @@ package com.test.two;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 
 public class NewTest {
-  @Test
+  @Test(groups={"orange"})
   public void test_001() {
 	  System.out.println("这个是@Test001方法！");
   }
-  @Test
+  @Test(groups={"orange"})
   public void test_002() {
 	  System.out.println("这个是@Test002方法！");
+  }
+  @Test(groups={"banana"})
+  public void test_003(){
+	  System.out.println("这个是@Test003方法");
+  }
+  @Test(groups={"apple"})
+  public void test_004(){
+	  System.out.println("这个是@Test004方法");
+  }
+  @Test(groups={"apple"})
+  public void test_005(){
+	  System.out.println("这个是@Test005方法");
+  }
+  @BeforeMethod
+  public void beforeMethod(){
+	  System.out.println("这个是@beforeMethod方法");
+  }
+  @AfterMethod
+  public void afterMethod(){
+	  System.out.println("这个是@afterMethod方法");
   }
   @BeforeClass
   public void beforeClass() {
